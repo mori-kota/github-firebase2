c1fce457c76eca5460e5458ad238641ae1c0992f
mori-kota <idein.sky@gmail.com>
2024-01-15 10:41:43 +0900

    no message

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from 'firebase-functions';
 
 export const testFunction = functions.https.onRequest((request: functions.Request, response: functions.Response) => {
+  console.log(request.body);
     const data = request.body.data;
 
     // Numbers passed from the client.
@@ -10,6 +11,8 @@ export const testFunction = functions.https.onRequest((request: functions.Reques
     const res = {
       Result: firstNumber * secondNumber,
     }
+
+    console.log(res);
 
     response.status(200).send(res);
 });
